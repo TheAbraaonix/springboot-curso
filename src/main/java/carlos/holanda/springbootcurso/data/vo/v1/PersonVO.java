805391315e30bc6d99c1321838fe74beedc29a1d
TheAbraaonix <carlosabraao@hotmail.com.br>
2024-04-04 11:23:57 -0300

    section 12 commit

--- src/main/java/carlos/holanda/springbootcurso/data/vo/v1/PersonVO.java
+++ src/main/java/carlos/holanda/springbootcurso/data/vo/v1/PersonVO.java
@@ -1,15 +1,23 @@
 package carlos.holanda.springbootcurso.data.vo.v1;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+//@JsonPropertyOrder({"id", "address", "gender", "first_name", "last_name"})
 public class PersonVO implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Long id;
+    //@JsonProperty("first_name")
     private String firstName;
+    //@JsonProperty("last_name")
     private String lastName;
     private String address;
+    //@JsonIgnore
     private String gender;
 
     public PersonVO() {}
