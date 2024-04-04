13a31353293e95e4d470d1c52a33113ce4384db2
TheAbraaonix <carlosabraao@hotmail.com.br>
2024-04-04 11:18:06 -0300

    section 11 commit

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` VALUES
+(6,'Ayrton','Senna','São Paulo, Brasil','Male'),
+(7,'Leonardo','da Vinci','Anchiano, Italy','Male'),
+(8,'Nikola','Tesla','Smiljan, Croatia','Male'),
+(9,'Anne','Frank','Frankfurt, Germany','Female');
